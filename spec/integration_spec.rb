06bd6638992fd68f24857ad9492b09498ce0f6f3
urso <steffen.siering@elastic.co>
2015-09-18 04:00:52 +0200

    add json data frame to protocol
    - add support for sending and receiving json data-frame
    - for server implementors to distinguish between normal data frame and
    json data frame the callback will be called with additional tag (:data,
    :json) depending on received message type.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -34,7 +34,7 @@ describe "A client" do
         tcp_server.accept do |socket|
           con = Lumberjack::Connection.new(socket)
           begin
-            con.run { |data| queue << data }
+            con.run { |_, data| queue << data }
           rescue
             # Close connection on failure. For example SSL client will make
             # parser for TCP based server trip.
@@ -45,7 +45,7 @@ describe "A client" do
     end
 
     @ssl_server = Thread.new do
-      ssl_server.run { |data| queue << data }
+      ssl_server.run { |_, data| queue << data }
     end
   end
 
